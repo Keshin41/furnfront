3d54721b62de9ceb9caf21f1e875eeea5334e382
KochyOkami <kochyokami@gmail.com>
2026-03-29 19:12:55 +0200

    feat(button): Introduce CustomButton component and replace UButton instances in index.vue

---
+++ app/components/CustomButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <UButton
+    :to="to"
+    :href="href"
+    :class="buttonClass"
+    :size="size"
+    @click="onClick"
+    v-bind="extraAttrs"
+  >
+    <slot>{{ label }}</slot>
+  </UButton>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useAttrs } from 'vue'
+
+type Size = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+
+const props = defineProps({
+  to: { type: String, required: false },
+  href: { type: String, required: false },
+  label: { type: String, required: false, default: '' },
+  bgClass: { type: String, required: false, default: 'bg-brand-yellow' },
+  textClass: { type: String, required: false, default: 'text-brand-green' },
+  size: { type: String as () => Size, required: false, default: 'lg' },
+  hoverClass: { type: String, required: false, default: '' },
+})
+
+const attrs = useAttrs()
+const emit = defineEmits(['click'])
+
+const buttonClass = computed(() => {
+  const base = 'rounded-full font-semibold shadow-lg px-8 py-3'
+  return [base, props.bgClass, props.textClass, props.hoverClass].filter(Boolean).join(' ')
+})
+
+const extraAttrs = attrs
+
+const onClick = (event: Event) => {
+  emit('click', event)
+}
+</script>
