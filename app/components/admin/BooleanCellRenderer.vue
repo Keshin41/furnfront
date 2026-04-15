15a6c831bd38303db2ffdc3ae8ece2eeabdfe4f2
Vikari <vgorgeon@gmail.com>
2026-04-15 20:35:45 +0200

    feat(admin): adding tickets visualization

---
+++ app/components/admin/BooleanCellRenderer.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const props = defineProps<{
+  data: boolean;
+}>();
+</script>
+<template>
+  <UBadge :color="props.data ? 'success' : 'error'" variant="outline">
+    {{ props.data ? "Oui" : "Non" }}
+  </UBadge>
+</template>
